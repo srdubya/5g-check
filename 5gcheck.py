30e1ec6bd9b5b8c7df9af48985e0ce1a7053c111
Steve Wiley <steve.wiley@cradlepoint.com>
2022-10-17 10:53:47 -0600

    Deleting comments/dead code.

--- 5gcheck.py
+++ 5gcheck.py
@@ -5,29 +5,9 @@ import sys
 import time
 
 import requests as requests
-# import rsa
-# import urllib
-# from rsa import PublicKey
-
-public_key = '''-----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDGKAHl+/ayHj031E/l/08ScLFb
-p9/jcLqAQJjbq6IDXiLeo23gXmlf3OwMpQlmQQLuOOBEizCHXKOgXn7HIfmonV5P
-qaCV72ot6N4ZO4FnYtdbun91rAYb1wq6s2Uyu6JuBomeMViapw4TPKv37GqLoAeF
-HNHwtHyPIJ14pT8HywIDAQAB
------END PUBLIC KEY-----
-'''
 
 
 def main():
-    # pub_key = PublicKey.load_pkcs1_openssl_pem(bytes(public_key, 'utf-8'))
-    # username = rsa.encrypt(bytes('admin', 'utf8'), pub_key)
-    # password = rsa.encrypt('Won4der2.'.encode('utf8'), pub_key)
-    # resp = requests.post("http://192.168.0.1/cgi-bin/luci", data=urllib.parse.urlencode({
-    #     'luci_username': username,
-    #     'luci_password': password
-    # }), headers={
-    #     'Content-Type': 'application/x-www-form-urlencoded'
-    # })
     if len(sys.argv) < 2:
         print("Please add the authentication cookie.")
         print(f"Usage:  {os.path.basename(sys.argv[0])} <authorization cookie>")
@@ -56,6 +36,5 @@ def main():
         sys.stdout.flush()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
