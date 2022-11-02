114ca31a33014ee7eab0bfa92c666aa9b37a2ac5
Steve Wiley <steve.wiley@cradlepoint.com>
2022-11-02 14:31:24 -0600

    Initial commit of MacOS app.

---
+++ macos/5G-Check/5G-Check/ChartData.swift
@@ -0,0 +1,64 @@
+//
+//  ChartData.swift
+//  5G-Check
+//
+//  Created by Steve Wiley on 10/30/22.
+//
+
+import Foundation
+
+class ChartData {
+    var name: String
+    var values: [String:Int]
+    
+    init(name: String) {
+        self.name = name
+        self.values = [:]
+    }
+    
+    func incrementValue(withKey: String) {
+        let present = self.values.contains { $0.key == withKey }
+        if present {
+            self.values.updateValue(self.values[withKey]! + 1, forKey: withKey)
+        } else {
+            self.values[withKey] = 1
+        }
+    }
+    
+    static func example0() -> ChartData {
+        let ret = ChartData(name: "rsrp")
+        for _ in 1...1000 {
+            ret.incrementValue(withKey: "-74")
+            ret.incrementValue(withKey: "-78")
+        }
+        for _ in 1...500 {
+            ret.incrementValue(withKey: "-76")
+            ret.incrementValue(withKey: "-79")
+        }
+        for _ in 1...50 {
+            ret.incrementValue(withKey: "-77")
+            ret.incrementValue(withKey: "-1,024")
+        }
+        return ret
+    }
+    
+    static func example1() -> ChartData {
+        let ret = ChartData(name: "signal")
+        for _ in 1...1000 {
+            ret.incrementValue(withKey: "-72")
+            ret.incrementValue(withKey: "-74")
+            ret.incrementValue(withKey: "-78")
+        }
+        for _ in 1...500 {
+            ret.incrementValue(withKey: "-76")
+            ret.incrementValue(withKey: "-79")
+        }
+        for _ in 1...50 {
+            ret.incrementValue(withKey: "-73")
+            ret.incrementValue(withKey: "-77")
+            ret.incrementValue(withKey: "-80")
+            ret.incrementValue(withKey: "-1,024")
+        }
+        return ret
+    }
+}
---
+++ macos/5G-Check/5G-Check/ContentView.swift
@@ -0,0 +1,102 @@
+//
+//  ContentView.swift
+//  5G-Check
+//
+//  Created by Steve Wiley on 10/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @Environment(\.openWindow) private var openWindow
+    @EnvironmentObject var gatewayState: GatewayState
+    let numCharts = 3.0
+    let width = 1.0
+    let vMargin = 1.0
+    let hSpace = 3.0
+    let margin = 1.0
+    let numberHeight = NSAttributedString("8").boundingRect(
+        with: CGSize(width: 300, height: 300),
+        options: [],
+        context: nil
+    ).height
+    let gutter = NSAttributedString("8").boundingRect(
+        with: CGSize(width: 300, height: 300),
+        options: [],
+        context: nil
+    ).height
+    static let numberFormatter: NumberFormatter = {
+        let ret = NumberFormatter()
+        ret.numberStyle = .decimal
+        ret.usesGroupingSeparator = true
+        ret.groupingSeparator = ","
+        ret.groupingSize = 3
+        return ret
+    }()
+    static let colors = [
+        Color(red: 0.0, green: 0.5, blue: 0.0),
+        Color(red: 0.0, green: 0.5, blue: 0.5),
+        Color(red: 0.0, green: 0.5, blue: 0.5)
+    ]
+    
+    fileprivate func drawChart(_ size: CGSize, _ context: GraphicsContext, _ chart: ChartData, _ index: Double) {
+        let bars = chart.values
+        let graphHeight = (((size.height - vMargin) / numCharts) - vMargin - gutter)
+        let vOffset = (graphHeight + gutter) * index
+        if bars.values.count > 0 {
+            let barWidth = ((size.width + hSpace - width) / CGFloat(bars.count)) - (3 * width)
+            let scaleFactor = (graphHeight - gutter) / CGFloat(bars.values.max()!)
+            var x = width
+            for bar in bars.keys.sorted(by: {a, b in
+                if let aAsInt = Int(a) {
+                    if let bAsInt = Int(b) {
+                        return aAsInt > bAsInt
+                    }
+                }
+                return a > b
+            }) {
+                let barHeight = scaleFactor * CGFloat(bars[bar]!)
+                context.fill(
+                    Path(CGRect(
+                        origin: CGPoint(x: x, y: graphHeight - barHeight + vOffset),
+                        size: CGSize(width: barWidth, height: barHeight)
+                    )),
+                    with: .color(ContentView.colors[Int(index)])
+                )
+                context.draw(
+                    Text(bar),
+                    at: CGPoint(x: x + (barWidth / 2), y: graphHeight + vMargin + (numberHeight/2) + vOffset)
+                )
+                x += barWidth + hSpace
+            }
+        }
+        context.draw(Text(chart.name).bold(), at: CGPoint(x: width, y: vOffset + numberHeight), anchor: .leading)
+    }
+    fileprivate func drawCharts(_ size: CGSize, _ context: GraphicsContext) {
+        drawChart(size, context, gatewayState.rsrp, 0)
+        drawChart(size, context, gatewayState.signal, 1)
+        drawChart(size, context, gatewayState.connectionType, 2)
+    }
+    
+    var body: some View {
+        VStack(
+            alignment: .center,
+            spacing: 10
+        ) {
+            Canvas { context, size in
+                drawCharts(size, context)
+            }
+            HStack {
+                TextField("token", text: $gatewayState.token, prompt: Text("auth token string"))
+                Button("Reset", action: {})
+            }
+        }
+        .padding(.all, 4.0)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().frame(width: 300, height: 300)
+    }
+}
